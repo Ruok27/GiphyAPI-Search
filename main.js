512558f8913c9c67cdfd00cd640c59676847a902
ruok27 <jakxii27@gmail.com>
2019-10-24 21:40:34 -0400

    10 gifs will now be added to the gif div according to the respective button, need to format the gifs and fix issue where inputted buttons are not appended

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-//id gifSpace
- let topics  = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "birds"];
- let buttonSpace = $("#buttonSpace")
+let topics = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "birds"];
+let buttonSpace = $("#buttonSpace");
+let gifSpace = $("#gifSpace");
 
 
 
 
 
-//let rating = $("<p>").text("Rating: " + results[i].rating);
 
 
-for (let i = 0; i < topics[i].length; i++){
+for (let i = 0; i < topics[i].length; i++) {
 
-    let p = $("<p>").text(topics[i]);
+    let p = $("<button>").text(topics[i]);
+    p.attr("class", "buttons");
     buttonSpace.append(p);
 
 
 
 
 
-//let r = $("<button type=\"button\" class=\"btn btn-primary btn-sm\">Small button</button>");
 
 
 }
@@ -28,17 +27,100 @@ for (let i = 0; i < topics[i].length; i++){
 
 
 
-$("#submitButton").on("click", function(e) {
+$("#submitButton").on("click", function () {
 
-    
+
 
     let input = $("#textInput").val().trim();
 
-    let p = $("<p>" +input+ "</p>");
 
+    let p = $("<button>").text(input);
+    p.attr("class", "buttons");
     topics.push(input);
 
     buttonSpace.append(p);
+
+
+})
+
+
+$(".buttons").on("click", function () {
+
+    gifSpace.empty();
+
+    // New buttons submitted dont work 
+    let topic = $(this).text();
+    const apiKey = "8sFzSG9nIt9QPYmHBa7A7inN6nBZSXpR";
+
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + apiKey + "&limit=10";
+
+
+$.ajax({
+
+url: queryURL,
+method: "GET"
+
+}).then(function(responce){
+
+    let getBack =  responce.data;
+
+//code here
+
+
+
+for (var i = 0; i < getBack.length; i++) {
+
+    if (getBack[i].rating !== "r" && getBack[i].rating !== "pg-13") {
+    
+    
+    
+    
+    
+        let actualGif = $("<img>");
+        actualGif.attr("src", getBack[i].images.fixed_height.url);
+
+
+        gifSpace.append(actualGif);
+
+    
+
+
+
+
+
+
+    
+    }
+
+}
+
+
+
+
+
+
+
+});
+
+
+//=============================================
+   // let q = $(this).text();
+
+ //   let p = $("<button>" + q + "</button>");
+
+  //  gifSpace.append(p);
+//==============================================
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
