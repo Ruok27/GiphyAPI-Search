f2d5a3e12d390d0a31275bfa73661e4ee2e027f0
ruok27 <jakxii27@gmail.com>
2019-10-25 03:20:44 -0400

    dynamicly added buttons will have their intended on click event occur

--- main.js
+++ main.js
@@ -8,27 +8,7 @@ let gifSpace = $("#gifSpace");
 
 
 
-for (let i = 0; i < topics[i].length; i++) {
-
-    let p = $("<button>").text(topics[i]);
-    p.attr("class", "buttons");
-    buttonSpace.append(p);
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
 $("#submitButton").on("click", function () {
-
 
 
     let input = $("#textInput").val().trim();
@@ -44,54 +24,16 @@ $("#submitButton").on("click", function () {
 })
 
 
-$(".buttons").on("click", function () {
-
-    gifSpace.empty();
-
-    // New buttons submitted dont work 
-    let topic = $(this).text();
-    const apiKey = "8sFzSG9nIt9QPYmHBa7A7inN6nBZSXpR";
-
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + apiKey + "&limit=10";
-
-
-$.ajax({
-
-url: queryURL,
-method: "GET"
-
-}).then(function(responce){
-
-    let getBack =  responce.data;
-
-//code here
-
-
-
-for (var i = 0; i < getBack.length; i++) {
-
-    if (getBack[i].rating !== "r" && getBack[i].rating !== "pg-13") {
-    
-    
-    
-    
-    
-        let actualGif = $("<img>");
-        actualGif.attr("src", getBack[i].images.fixed_height.url);
-
-
-        gifSpace.append(actualGif);
-
-    
 
 
 
 
 
+for (let i = 0; i < topics[i].length; i++) {
 
-    
-    }
-
+    let p = $("<button>").text(topics[i]);
+    p.attr("class", "buttons");
+    buttonSpace.append(p);
 }
 
 
@@ -100,21 +42,52 @@ for (var i = 0; i < getBack.length; i++) {
 
 
 
-});
-
-
-//=============================================
-   // let q = $(this).text();
-
- //   let p = $("<button>" + q + "</button>");
-
-  //  gifSpace.append(p);
-//==============================================
 
 
 
 
 
+$(document).on("click", ".buttons", function () {
+
+    gifSpace.empty();
+
+
+
+    // New buttons submitted dont work 
+    let topic = $(this).text();
+    //remember to use that document.ready thing after elements are created on a page
+
+    const apiKey = "8sFzSG9nIt9QPYmHBa7A7inN6nBZSXpR";
+
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + apiKey + "&limit=10";
+
+
+    $.ajax({
+
+        url: queryURL,
+        method: "GET"
+
+    }).then(function (responce) {
+
+        let getBack = responce.data;
+
+
+
+
+
+        for (var i = 0; i < getBack.length; i++) {
+
+            if (getBack[i].rating !== "r" && getBack[i].rating !== "pg-13") {
+
+
+
+
+
+                let actualGif = $("<img>");
+                actualGif.attr("src", getBack[i].images.fixed_height.url);
+
+
+                gifSpace.append(actualGif);
 
 
 
@@ -123,4 +96,18 @@ for (var i = 0; i < getBack.length; i++) {
 
 
 
+                //end if statement
+            }
+
+            //end for loop
+        }
+
+
+
+
+
+
+        //end then function
+    });
+    //end on button click
 })
